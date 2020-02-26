5b88a9f94e0c4e4717f8431991d671719ac1995e
Yoshihiro Tanaka <contact@cordea.jp>
2020-02-26 22:13:09 +0900

    add tab

--- app/src/main/java/jp/cordea/ikameshi/MainActivity.kt
+++ app/src/main/java/jp/cordea/ikameshi/MainActivity.kt
@@ -3,31 +3,47 @@ package jp.cordea.ikameshi
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
-import androidx.ui.core.Text
 import androidx.ui.core.setContent
+import androidx.ui.layout.Column
+import androidx.ui.layout.Container
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.Tab
+import androidx.ui.material.TabRow
 import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MaterialTheme {
-                Greeting("Android")
-            }
-        }
+        setContent { View() }
     }
 }
 
+enum class MainTab(val title: String) {
+    HOME("Home"),
+    DRAFT("Draft"),
+    LIKE("Like")
+}
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun View() {
+    MaterialTheme {
+        Column {
+            Container(modifier = LayoutFlexible(1f)) {
+            }
+            TabRow(items = MainTab.values().asList(), selectedIndex = 0, tab = { index, tab ->
+                Tab(
+                        text = tab.title,
+                        selected = tab.ordinal == index,
+                        onSelected = {
+                        }
+                )
+            }, indicatorContainer = {})
+        }
+    }
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
-    MaterialTheme {
-        Greeting("Android")
-    }
+    View()
 }
