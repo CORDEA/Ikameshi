cbb4f5d4500e760e23debfa51f8842c11964d191
Yoshihiro Tanaka <contact@cordea.jp>
2020-02-28 00:00:13 +0900

    add card

--- app/src/main/java/jp/cordea/ikameshi/AlbumScreen.kt
+++ app/src/main/java/jp/cordea/ikameshi/AlbumScreen.kt
@@ -4,8 +4,12 @@ import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.foundation.Clickable
 import androidx.ui.foundation.VerticalScroller
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
+import androidx.ui.graphics.Color
 import androidx.ui.layout.*
 import androidx.ui.material.ripple.Ripple
+import androidx.ui.material.surface.Card
+import androidx.ui.material.surface.Surface
 import androidx.ui.text.TextStyle
 import androidx.ui.text.font.FontWeight
 import androidx.ui.unit.dp
@@ -16,24 +20,31 @@ object AlbumScreen {
     fun Item() {
         Ripple(bounded = true) {
             Clickable(onClick = {}) {
-                Container(padding = EdgeInsets(16.dp)) {
-                    Row(arrangement = Arrangement.Center) {
-                        Text(
-                            text = "",
-                            modifier = LayoutGravity.Center + LayoutPadding(right = 16.dp)
-                        )
+                Card(modifier = LayoutPadding(16.dp), shape = RoundedCornerShape(12.dp)) {
+                    Container {
                         Column {
+                            Surface(color = Color.Blue) {
+                                Container(modifier = LayoutAspectRatio(1.7f)) {
+                                }
+                            }
+                            Spacer(modifier = LayoutHeight(16.dp))
                             Text(
-                                text = "",
+                                text = "Album",
                                 style = TextStyle(
-                                    fontSize = 16.sp,
+                                    fontSize = 24.sp,
                                     fontWeight = FontWeight.Bold
-                                )
+                                ),
+                                modifier = LayoutPadding(left = 16.dp, right = 16.dp)
                             )
                             Text(
-                                text = "",
-                                style = TextStyle(fontSize = 12.sp),
-                                modifier = LayoutPadding(top = 4.dp)
+                                text = "Description",
+                                style = TextStyle(fontSize = 16.sp),
+                                modifier = LayoutPadding(
+                                    top = 4.dp,
+                                    right = 16.dp,
+                                    bottom = 16.dp,
+                                    left = 16.dp
+                                )
                             )
                         }
                     }
@@ -44,7 +55,8 @@ object AlbumScreen {
 
     @Composable
     fun View() {
-        VerticalScroller {
+        VerticalScroller(modifier = LayoutHeight.Fill) {
+            Item()
         }
     }
 }
