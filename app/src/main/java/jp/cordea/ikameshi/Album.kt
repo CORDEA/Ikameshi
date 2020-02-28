131398385825bda8b0dc4ad180a45b626f9bb203
Yoshihiro Tanaka <contact@cordea.jp>
2020-02-29 01:01:23 +0900

    rename

--- app/src/main/java/jp/cordea/ikameshi/Album.kt
+++ app/src/main/java/jp/cordea/ikameshi/Album.kt
@@ -2,7 +2,7 @@ package jp.cordea.ikameshi
 
 class Album(
     val id: Long,
-    val name: String,
+    val title: String,
     val artist: Artist,
     val thumbnailUrl: String,
     val releaseYear: String
