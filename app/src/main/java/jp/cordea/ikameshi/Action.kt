d270a54aec25cac181f172550057939ce0f5c8ec
Yoshihiro Tanaka <contact@cordea.jp>
2020-02-29 22:33:44 +0900

    define actions

--- app/src/main/java/jp/cordea/ikameshi/Action.kt
+++ app/src/main/java/jp/cordea/ikameshi/Action.kt
@@ -3,4 +3,6 @@ package jp.cordea.ikameshi
 sealed class Action {
     class ChangeTab(val tab: Tab) : Action()
     class FetchMusics(val musics: List<Music>) : Action()
+    class LikeMusic(val id: Long) : Action()
+    class UnlikeMusic(val id: Long) : Action()
 }
