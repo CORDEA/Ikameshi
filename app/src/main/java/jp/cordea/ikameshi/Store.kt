99ab1f92a868b6f4d31c7411eff48adefe2c8cc3
Yoshihiro Tanaka <contact@cordea.jp>
2020-02-29 16:40:31 +0900

    subscribe only once

--- app/src/main/java/jp/cordea/ikameshi/Store.kt
+++ app/src/main/java/jp/cordea/ikameshi/Store.kt
@@ -1,23 +1,23 @@
 package jp.cordea.ikameshi
 
 import androidx.compose.Composable
-import io.reactivex.android.schedulers.AndroidSchedulers
+import androidx.compose.onActive
 import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.subscribeBy
 
 class Store(
     private val reducer: Reducer
 ) {
-    private val state = State()
+    private var state = State()
     private lateinit var disposable: Disposable
 
     @Composable
     fun provide(view: @Composable() (State) -> Unit) {
+        onActive {
+            disposable = reducer.reduce(state)
+                .subscribeBy { state.mainState = it.mainState }
+        }
         view(state)
-        disposable = reducer.reduce()
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeBy {
-            }
     }
 
     fun dispose() {
