c695a342f03d924e370cc8e3e174ff88d6d15164
Yoshihiro Tanaka <contact@cordea.jp>
2020-03-01 20:31:16 +0900

    observe

--- app/src/main/java/jp/cordea/ikameshi/FavoriteScreen.kt
+++ app/src/main/java/jp/cordea/ikameshi/FavoriteScreen.kt
@@ -1,14 +1,46 @@
 package jp.cordea.ikameshi
 
 import androidx.compose.Composable
+import androidx.compose.onActive
+import androidx.compose.onDispose
 import androidx.ui.foundation.VerticalScroller
 import androidx.ui.layout.LayoutHeight
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.SerialDisposable
+import io.reactivex.rxkotlin.subscribeBy
 
 class FavoriteScreen(
+    private val store: MusicStore,
     private val listItem: MusicListItem
 ) {
+    private val serialDisposable = SerialDisposable()
+
     @Composable
     fun View(state: FavoriteState) {
+        onActive {
+            store.onFavoriteMusicChanged()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy { result ->
+                    when (result) {
+                        MusicResult.FetchFavoriteMusics.Loading -> {
+                        }
+                        is MusicResult.FetchFavoriteMusics.Success ->
+                            state.items = result.musics.map {
+                                MusicItemState(
+                                    it.id,
+                                    it.title,
+                                    it.liked
+                                )
+                            }
+                        MusicResult.FetchFavoriteMusics.Failure ->
+                            state.items = emptyList()
+                    }
+                }
+                .run { serialDisposable.set(this) }
+        }
+        onDispose {
+            serialDisposable.dispose()
+        }
         VerticalScroller(modifier = LayoutHeight.Fill) {
             state.items.forEach { listItem.View(it) }
         }
