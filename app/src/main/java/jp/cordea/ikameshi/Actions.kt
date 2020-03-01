8c02fcf19191a3b9f22e0ae3ebc90e291eabae8b
Yoshihiro Tanaka <contact@cordea.jp>
2020-03-01 14:43:19 +0900

    implement actions

--- app/src/main/java/jp/cordea/ikameshi/Actions.kt
+++ app/src/main/java/jp/cordea/ikameshi/Actions.kt
@@ -2,8 +2,12 @@ package jp.cordea.ikameshi
 
 class Actions(
     private val changeTab: ChangeTab,
-    private val fetchMusics: FetchMusics
+    private val likeMusic: LikeMusic,
+    private val fetchMusics: FetchMusics,
+    private val unlikeMusic: UnlikeMusic
 ) {
     fun changeTab(tab: Tab) = changeTab.dispatch(tab)
     fun fetchMusics() = fetchMusics.dispatch()
+    fun likeMusic(id: Long) = likeMusic.dispatch(id)
+    fun unlikeMusic(id: Long) = unlikeMusic.dispatch(id)
 }
